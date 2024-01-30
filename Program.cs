f8cd333572a500c4750924bbaaccd436686cf6b8
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-30 18:02:16 +0530

    C3-Fibonacci-Series branch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿using System;
-Console.WriteLine ("Hello, World!");
+using System;
+for (; ; ) {
+   Console.Write ("Enter the number: ");
+   int firstNum = 0, secondNum = 1, nextNum, num;
+   if (int.TryParse (Console.ReadLine (), out num)) {
+      if (num < 2) {
+         Console.Write ("Please Enter a number greater than two");
+      } else {
+         Console.Write (firstNum + " " + secondNum + " ");
+         for (int i = 2; i < num; i++) {
+            nextNum = firstNum + secondNum;
+            Console.Write (nextNum + " ");
+            firstNum = secondNum;
+            secondNum = nextNum;
+         }
+      }
+   } else Console.WriteLine ("Enter Valid number");
+}
