d7478d93738b1ca1bb7dfb51974d3ae77b11e12a
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-30 18:04:57 +0530

    C4: GCD And LCM

--- Program.cs
+++ Program.cs
@@ -1,29 +1,20 @@
 using System;
 for (; ; ) {
-   Console.Write ("Enter a decimal number: ");
-   if (int.TryParse (Console.ReadLine (), out int decimalnum)) {
-      if (decimalnum > 0) {
-         string[] hexacha = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
-         string hexadeci = "";
-         int num = decimalnum;
-         while (num > 0) {
-            string reminder = hexacha[num % 16];
-            hexadeci = reminder + hexadeci;
-            num /= 16;
-         }
-         Console.WriteLine ($"Hexadecimal: {hexadeci}");
-         string binary = "";
-         while (decimalnum > 0) {
-            int remainder = decimalnum % 2;
-            binary = remainder.ToString () + binary;
-            decimalnum /= 2;
-         }
-         Console.WriteLine ($"Binary:{binary}");
-      }
-      else {
-         Console.WriteLine ($"Binary : 00000000");
-         Console.WriteLine ($"Hexadecimal : 0");
-      }
-   } 
-   else Console.WriteLine ("enter valid number");
+   Console.Write ("Enter the first number: ");
+   int.TryParse (Console.ReadLine (), out int a);
+   Console.Write ("Enter the second number: ");
+   int.TryParse (Console.ReadLine (), out int b);
+   int gcd = CalculateGCD (a, b);
+   int lcm = (a * b) / gcd;
+   Console.WriteLine ($"GCD of {a} and {b} is: {gcd}");
+   Console.WriteLine ($"LCM of {a} and {b} is: {lcm}");
+}
+
+int CalculateGCD (int X, int Y) {
+   while (Y != 0) {
+      int temp = Y;
+      Y = X % Y;
+      X = temp;
+   }
+   return X;
 }
