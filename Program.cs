377da4ad1c44b29d7454392466c5035f5a5d1781
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-30 18:11:23 +0530

    C6: Palindrome Checker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 using System;
-Console.WriteLine ("Hello World!");
+Console.Write ("Enter a word or phrase:");
+string text = Console.ReadLine ();
+string original = text.Replace (" " , "").ToLower ();
+if (original.Equals (ReverseString (original))) Console.WriteLine (text + " is a Palindrome!");
+else Console.WriteLine (text + " is not a Palindrome!");
+
+string ReverseString (string original) {
+   char[] chars = original.ToCharArray ();
+   string rev = "";
+   for (int i = chars.Length - 1; i >= 0; i--)
+      rev += chars[i];
+   return rev;
+}
