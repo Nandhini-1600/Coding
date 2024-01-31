07fb15f147a5b4f58618ff07c4813a4f31d0f023
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 12:59:09 +0530

    C20: Factorial Number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 using System;
-Console.WriteLine ("Hello World!");
+for (; ; ) {
+   Console.Write ("Enter the number : ");
+   if (int.TryParse (Console.ReadLine (), out int num)) Console.WriteLine ("Factorial number for " + num + "! is " + FactorialNumber (num));
+   else Console.WriteLine ("Invalid Input");
+}
+
+static long FactorialNumber (int num) {
+   long factorial = 1;
+   for (int i = 1; i <= num; i++) factorial *= i;
+   return factorial;
+}
