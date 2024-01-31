9ae4ed6ff2ba2e46fa1158f82cdd34a45dcc2293
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 11:47:31 +0530

    C10: Reverse a Given Number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 using System;
-Console.WriteLine ("Hello World!");
+for (; ; ) {
+   Console.Write ("Enter the number:");
+   if (int.TryParse (Console.ReadLine (), out int number)) {
+      int orgNumber = number;
+      int revNumber = 0;
+      while (number > 0) {
+         int lastDigit = number % 10;
+         revNumber = revNumber * 10 + lastDigit;
+         number /= 10;
+      }
+      if (revNumber == orgNumber) Console.WriteLine ("It is a Palindrome");
+      else Console.WriteLine ("It is not a Palindrome");
+   }
+   else Console.WriteLine ("Please enter valid input.");
+}
