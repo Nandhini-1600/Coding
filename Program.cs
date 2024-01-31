5fed119935e23848fa7f51d754aeeb4f61389a2b
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 13:00:24 +0530

    C21: Display The Individual Digits Of a Given Number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 using System;
-Console.WriteLine ("Hello World!");
+Console.Write ("Enter the number:");
+string input = Console.ReadLine ();
+Console.WriteLine (IndividualDigit (input));
+
+static string IndividualDigit (string input) {
+   if (input.Contains ('.')) {
+      string[] substrings = input.Split ('.');
+      if (substrings.Length == 2) {
+         string integralPart = string.Join (" ", substrings[0].ToCharArray ());
+         string fractionalPart = string.Join (" ", substrings[1].ToCharArray ());
+         return "Integral part: " + integralPart + "; Fractional part: " + fractionalPart;
+      }
+      return "Invalid input";
+   }
+   return "Integral part:" + string.Join (" ", input.ToCharArray ());
+}
