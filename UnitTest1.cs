084a8dfc1c0c0f663697c5d38e71038ca16b6e12
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 17:30:23 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for interns at Metamation, Batch - July 2023.
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------
+// Program.cs
+// A10.1:
+// Test cases for filename parser.
+// ------------------------------------------------------------------------------------------------
+namespace Training {
+   [TestClass]
+   public class UnitTest1 {
+      [TestMethod]
+      public void TestMethod1 () {
+         var tests = new Dictionary<string, bool> {
+                { @"Cz:\abc\def\r.txt", false },
+                { @"C:\abc\def\r.txt", true },
+                { @"C:\Readme.txt", false },
+                { @"C:\abc\.bcf", false },
+                { @"C:\abc\bcf.", false },
+                { @"Readme.txt", false },
+                { @"C:\abc\def", false },
+                { @"C:\abc d", false },
+                { @"\abcd\Readme.txt", false },
+                { " ", false },
+                { @"C:\ab.c\def\r.txt", false },
+                { @"C:\abc:d", false },
+                { @".\abc", false },
+                { ".abc", false },
+                { "abc", false },
+                { @"C:\abc6\def\r.txt", false },
+                { @"C:\work\r.txt", true },
+                { @"C:\abc\def\r.txt.txt", false },
+                { @"C:\Program Files\<>*&^%$#@!.txt", false },
+                { @"C:\work~\r.txt~", false }
+         };
+         var fileParse = new FileParse ();
+         foreach (var testCase in tests) {
+            bool parseResult = fileParse.FileNameParse (testCase.Key, out _);
+            Assert.AreEqual (testCase.Value, parseResult);
+         }
+      }
+   }
+}
