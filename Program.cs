87157bc83cf6816a1d0e5bac337a73544f61f6d0
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 12:45:58 +0530

    C14: Reduced String

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 using System;
-Console.WriteLine ("Hello World!");
+using System.Text;
+for (; ; ) {
+   Console.Write ("Enter the input string:");
+   string input = Console.ReadLine ().ToLower ();
+   string reducedString = ReduceString (input);
+   Console.WriteLine ("Output:" + reducedString);
+}
+
+string ReduceString(string input) {
+   StringBuilder result = new StringBuilder ();
+   foreach (char c in input) {
+      if (result.Length > 0 && result[result.Length - 1] == c) result.Remove (result.Length - 1, 1);
+      else result.Append (c);
+   }
+   return result.ToString ();
+}
