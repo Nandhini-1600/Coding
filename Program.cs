8ea85b9dc1c19f2bb00476348c24a12fe370f36c
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 13:01:17 +0530

    C22: Swap a Given Number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 using System;
-Console.WriteLine ("Hello World!");
+for (; ; ) {
+   Console.Write ("Enter the number 1 :");
+   int.TryParse (Console.ReadLine (), out int num1);
+   Console.Write ("Enter the number 2 :");
+   int.TryParse (Console.ReadLine (), out int num2);
+   SwapNumber (ref num1, ref num2);
+   Console.WriteLine ($"After swapping :num1={num1},num2={num2}");
+}
+
+static void SwapNumber (ref int num1, ref int num2) {
+   int temp = num1;
+   num1 = num2;
+   num2 = temp;
+}
