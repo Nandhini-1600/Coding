c8fb5a7f1cf047f6d270cbc0eaa48711b2d9a9cd
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 16:16:31 +0530

    P1: Number guessing game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 using System;
-Console.WriteLine ("Hello World!");
+int random_num = new Random ().Next (1, 101);
+for (; ; ) {
+   Console.Write ("Enter your guess:");
+   bool var = int.TryParse (Console.ReadLine (), out int guess_num);
+   if (var == false) Console.WriteLine ("Invalid guess");
+   else {
+      if (guess_num > random_num) Console.WriteLine ("Your guess is too high.");
+      else if (guess_num < random_num) Console.WriteLine ("Your guess is too low.");
+      else {
+         Console.WriteLine ("Your guessed correctly.");
+         break;
+      }
+   }
+}
