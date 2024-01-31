17438d32d5a90cb24c7ddd5cffa9d3d7798ed4ea
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-01-31 12:48:42 +0530

    C16; Longest Abecedarian Word

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 using System;
-Console.WriteLine ("Hello World!");
+using System.Linq;
+string[] words = { "apple", "effort", "cherry", "glory", "fig", "grape", "aegilops", "adopt", "mother", "office" };
+string longestAbecedarianWord = FindLongestAbecedarianWord (words);
+var res = string.IsNullOrEmpty (longestAbecedarianWord) ? $"No abecedarian word found in the array." : $"Longest Abecedarian Word:{longestAbecedarianWord}";
+Console.WriteLine (res);
+
+bool IsAbecedarian (string word) => word == new string (word.OrderBy (c => c).ToArray ());
+
+string FindLongestAbecedarianWord (string[] words) {
+   string longestAbecedarianWord = "";
+   foreach (string word in words) if (IsAbecedarian (word) && word.Length > longestAbecedarianWord.Length) longestAbecedarianWord = word;
+   return longestAbecedarianWord;
+}
